de28a55324db43ffac040459dd9669825f9b55f0
Tim M <poad27xx@gmail.com>
2017-08-14 16:33:32 -0400

    Authentication and crud commands functional

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :set_item, only: [:show, :update, :destroy]
+  before_action :set_item, only: [:show, :destroy, :increase_quantity, :decrease_quantity]
 
   # GET /items
   def index
@@ -25,17 +25,36 @@ class ItemsController < ApplicationController
   end
 
   # PATCH/PUT /items/1
-  def update
-    if @item.update(item_params)
-      render json: @item
-    else
-      render json: @item.errors, status: :unprocessable_entity
-    end
-  end
+  #def update
+  #  if @item.update(item_params)
+  #    item.find_by(id: 1)
+  #    item.quantity = 1
+  #    item.save
+  #    render json: itm
+  #  else
+  #    render json: @item.errors, status: :unprocessable_entity
+  #  end
+  #end
 
   # DELETE /items/1
   def destroy
     @item.destroy
+  end
+
+  #http://localhost:3000/items/1/update_quantity?value=5
+
+  def increase_quantity
+    value = @item.quantity
+    value = value + params[:value].to_i
+    @item.update(:quantity => value)
+    render json: @item
+  end
+
+  def decrease_quantity
+    value = @item.quantity
+    value = value - params[:value].to_i
+    @item.update(:quantity => value)
+    render json: @item
   end
 
   private
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170714011202) do
+ActiveRecord::Schema.define(version: 20170717205945) do
 
   create_table "items", force: :cascade do |t|
     t.string "name"
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "quantity"
+    t.integer "min_stock"
   end
 
   create_table "users", force: :cascade do |t|
